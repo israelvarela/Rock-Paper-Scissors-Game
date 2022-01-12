6274ed8ccef3f8a487ff05a64f7c2d84e4637623
Izzy V <israelreyvarela@gmail.com>
2022-01-12 16:50:32 -0600

    added player choices logic

---
+++ app.js
@@ -0,0 +1,34 @@
+const playerScore = 0;
+const compScore = 0;
+const playerScore_span = document.getElementById("player-score");
+const compScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+}
+
+
+function game(playerChoice) {
+    
+}
+
+function main() {
+  rock_div.addEventListener("click", function () {
+    game("r");
+  });
+
+  paper_div.addEventListener("click", function () {
+    game("p");
+  });
+
+  scissors_div.addEventListener("click", function () {
+    game("s");
+  });
+}
+
+main();
